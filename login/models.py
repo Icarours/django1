5e232bd73d2833f96fe7cb21df684055903a7bc7
syl <j376787348@163.com>
2019-06-28 17:28:06 +0800

    2019年6月28日17:27:52 1.demo1

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 继承models.Model,固定写法.创建两个字段,最大长度32位,char类型
+class UserInfo(models.Model):
+    user = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=32)
--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from login import models
 
 # Create your views here.
 # 顶部增加一个空列表
@@ -14,10 +15,14 @@ def index(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        # 将数据爆粗难道数据库
+        models.UserInfo.objects.create(user=username, pwd=password)
         print(username, password)
         # 将用户名和面保存到一个字典中
-        temp = {'user': username, 'pwd': password}
+        # temp = {'user': username, 'pwd': password}
         # 将字典添加到列表中
-        user_list.append(temp)
+        # user_list.append(temp)
         # 将user_list列表作为上下文参数供render渲染到HTML
+    # 从数据库读取所有数据
+    user_list = models.UserInfo.objects.all()
     return render(request, 'index.html', {'data': user_list})
